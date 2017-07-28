59b6675d47d89119e26594fd6772b64fc5548b24
junsom <hjsactivity@gmail.com>
2017-07-28 17:28:22 +0800

    更新脚本

---
+++ md/Linux 常用指令.md
@@ -0,0 +1,2 @@
+## linux之间传输文件
+`scp -r   /home/wwwroot/www/charts/util root@192.168.1.65:/home/wwwroot/limesurvey_back/scp`
---
+++ script/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git_md_home="/junsom/git/hexo-md"
+git_md_dir="${git_md_home}/md"
+
+blog_home="/junsom/blog"
+blog_post_dir="${blog_home}/source/_posts"
+
+cd ${git_md_home}
+git pull
+
+cd ${blog_post_dir}
+rm rf *
+
+cp -r ${git_md_dir}/* .
+
+cd ${blog_home}
+
+hexo clean generate
+
